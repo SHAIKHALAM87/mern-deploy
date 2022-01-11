64d643d9fd4601eb29831326b736648b412f08d5
alam <shaikh.alam87@gmail.com>
2022-01-11 21:54:32 +0530

    git first commit

---
+++ frontend/src/Login.js
@@ -0,0 +1,50 @@
+import {React,useState} from 'react';
+import axios from 'axios';
+
+
+function Login(){
+
+    const [username,setusername] = useState({name:''});
+    const handleSubmit = (e)=>{
+      e.preventDefault();
+
+      if (username) {
+
+        console.log(username);
+
+        // const requestOptions = {
+        //     method: 'POST',
+        //     headers: { 'Content-Type': 'application/json' },
+        //     body: JSON.stringify({ username })
+        // };
+
+        axios.get('api/todos')
+        .then((res)=>{
+            console.log(res);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+      }
+  
+    };
+  
+    const handleChange = (e)=>{
+      e.preventDefault();
+
+      setusername({name: e.target.value});
+      
+    }
+
+    return(
+        <div>
+            <form name="form" onSubmit={handleSubmit}>
+                <input type="text" className="form-control" name="username" value={username['name']} onChange={handleChange} />
+                <button className="btn btn-primary">To-do</button>
+            </form>                         
+        </div>
+    );
+
+}
+
+export default Login;
